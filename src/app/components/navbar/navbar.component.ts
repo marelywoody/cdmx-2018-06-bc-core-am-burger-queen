370edd20eeab9eab8874fa1ee3c5f4f3bf0028dc
marelywoody <ros_ely_arc@hotmail.com>
2018-10-19 12:10:15 -0500

    salir de sesion

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthenticationService } from '../../services/authentication.service';
 
 @Component({
   selector: 'app-navbar',
@@ -7,9 +8,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  public isLogin: boolean;
+  public nameUser: string;
+  public emailUser: string;
+  public userId: string;
+
+  constructor( 
+    public authentication: AuthenticationService
+  ) { }
 
   ngOnInit() {
+    this.authentication.stateAuth().subscribe( auth => {
+      if (auth) {
+        this.isLogin = true;
+        this.emailUser = auth.email;
+        this.userId = auth.uid;
+      } else {
+        this.isLogin = false;
+      }
+    })
+  }
+
+  clickLogout() {
+    this.authentication.logout();
   }
 
 }
